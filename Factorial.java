4b0b631fdbe42bfbc8771264bb8e3f7b2ba65043
Raisul Islam <robin35.291@gmail.com>
2023-10-06 02:26:50 +0600

    third commit

--- Factorial.java
+++ Factorial.java
@@ -1,4 +1,4 @@
-public class Recursion2 {
+public class Factorial {
     public static int calculateFactorial(int n) {
         if (n == 1 || n == 0) {
             return 1;
@@ -7,7 +7,6 @@ public class Recursion2 {
         int fact_n = n * fact_nm1;
         return fact_n;
     }
-
     public static void main(String[] args) {
         int n = 5;
         int ans = calculateFactorial(n);
---
+++ Greetings.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class Greetings {
+    int count;
+
+    public void greet() {
+       if(count ==1_000){
+        return;
+       }
+       System.out.println("Hello, World");
+       count++;
+       greet();
+    }
+
+    public static void main(String[] args) {
+        Greetings greetings = new Greetings();
+        greetings.greet();
+    }
+}
